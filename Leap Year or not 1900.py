d82c5f541264bd860308abb4e8739b20f8bb4846
BinduReddy1610 <hima0714u@gmail.com>
2025-12-30 16:17:36 +0530

    Create Leap Year or not 1900.py

---
+++ Leap Year or not 1900.py
@@ -0,0 +1,10 @@
+year=int(input())
+if year%4==0 and year%100!=0:
+    print("leap year")
+elif year%400==0:
+    print("leap year")
+else:
+    print("not a leap year")
+OUTPUT:
+1900
+not a leap year
